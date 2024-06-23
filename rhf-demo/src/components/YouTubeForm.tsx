2d7a7f587b91ff92864cb213bd01e9f4e1297d3b
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-06-24 00:05:06 +0300

    Form Submission

--- rhf-demo/src/components/YouTubeForm.tsx
+++ rhf-demo/src/components/YouTubeForm.tsx
@@ -3,17 +3,28 @@ import { DevTool } from "@hookform/devtools";
 
 let renderCount = 0;
 
+type FormValues = {
+  username: string;
+  email: string;
+  channel: string;
+};
+
 const YouTubeForm = () => {
-  const form = useForm();
-  const { register, control } = form;
+  const form = useForm<FormValues>();
+  const { register, control, handleSubmit } = form;
   // const { name, ref, onChange, onBlur } = register("username");
+
+  const onSubmit = (data: FormValues) => {
+    console.log("Form is submitted.");
+    console.log(data);
+  };
 
   renderCount++;
   return (
     <div>
       <h1>YouTube Form {renderCount / 2}</h1>
 
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="username">Username</label>
         <input type="text" id="username" {...register("username")} />
 
