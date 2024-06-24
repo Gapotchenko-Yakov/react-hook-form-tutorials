49c37ba05edc2beff8e9768dd875d64ba11c8b28
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-06-24 12:16:02 +0300

    Return simple object defaultValues

--- rhf-demo/src/components/YouTubeForm.tsx
+++ rhf-demo/src/components/YouTubeForm.tsx
@@ -11,16 +11,10 @@ type FormValues = {
 
 const YouTubeForm = () => {
   const form = useForm<FormValues>({
-    defaultValues: async () => {
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/users/1"
-      );
-      const data = await response.json();
-      return {
-        username: "John",
-        email: data.email,
-        channel: "",
-      };
+    defaultValues: {
+      username: "John",
+      email: "",
+      channel: "",
     },
   });
   const { register, control, handleSubmit, formState } = form;
